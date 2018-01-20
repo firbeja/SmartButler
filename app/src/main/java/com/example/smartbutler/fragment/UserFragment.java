c542097e95e45d9aaf8a408164684a135136f054
Tony <13723786518@163.com>
2018-01-20 19:24:06 +0800

    3-7 用户管理-个人中心数据处理开发。补3-6的题目，他的题目是登录自定义Dialog开发。

--- app/src/main/java/com/example/smartbutler/fragment/UserFragment.java
+++ app/src/main/java/com/example/smartbutler/fragment/UserFragment.java
@@ -8,21 +8,137 @@ package com.example.smartbutler.fragment;
  * 描述：   TODO
  */
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.smartbutler.R;
+import com.example.smartbutler.entity.MyUser;
+import com.example.smartbutler.ui.LoginActivity;
 
-public class UserFragment extends Fragment {
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.UpdateListener;
 
-    @Nullable
+public class UserFragment extends Fragment implements View.OnClickListener {
+
+    private Button btn_exit_user;
+    private TextView edit_user;
+    private EditText et_username;
+    private EditText et_sex;
+    private EditText et_age;
+    private EditText et_desc;
+    private Button btn_update_ok;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user, null);
+        findView(view);
         return view;
+    }
+
+    private void findView(View view) {
+
+        btn_exit_user = view.findViewById(R.id.btn_exit_user);
+        btn_exit_user.setOnClickListener(this);
+
+        edit_user = view.findViewById(R.id.edit_user);
+        edit_user.setOnClickListener(this);
+
+        et_username = view.findViewById(R.id.et_username);
+        et_sex = view.findViewById(R.id.et_sex);
+        et_age = view.findViewById(R.id.et_age);
+        et_desc = view.findViewById(R.id.et_desc);
+
+        //默认是不可点击的/ 或者说是不可输入的
+        setEnabled(false);
+
+        // 设置当前的值
+        MyUser userInfo = BmobUser.getCurrentUser(MyUser.class);
+        et_username.setText(userInfo.getUsername());
+        et_sex.setText(userInfo.isSex()?"男":"女");
+        et_age.setText(userInfo.getAge() + "");
+        et_desc.setText(userInfo.getDesc());
+
+        btn_update_ok = view.findViewById(R.id.btn_update_ok);
+        btn_update_ok.setOnClickListener(this);
+
+    }
+
+    // 控制焦点
+    private void setEnabled(boolean is){
+        et_username.setEnabled(is);
+        et_sex.setEnabled(is);
+        et_age.setEnabled(is);
+        et_desc.setEnabled(is);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            //退出登录
+            case R.id.btn_exit_user :
+                MyUser.logOut();   //清除缓存用户对象
+                BmobUser currentUser = MyUser.getCurrentUser(); // 现在的currentUser是null了
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+                getActivity().finish();
+                break;
+            //编辑资料
+            case R.id.edit_user :
+                setEnabled(true);
+                btn_update_ok.setVisibility(View.VISIBLE);
+                break;
+            case R.id.btn_update_ok :
+                //1.拿到输入框的值
+                String username = et_username.getText().toString().trim();
+                String age = et_age.getText().toString().trim();
+                String sex = et_sex.getText().toString().trim();
+                String desc = et_desc.getText().toString().trim();
+
+                //2.判断是否为空
+                if (!TextUtils.isEmpty(username) & !TextUtils.isEmpty(age) & !TextUtils.isEmpty(sex)){
+                    //3.更新属性
+                    MyUser user = new MyUser();
+                    user.setUsername(username);
+                    user.setAge(Integer.parseInt(age));
+                    if (sex.equals("男")){
+                        user.setSex(true);
+                    }else {
+                        user.setSex(false);
+                    }
+                    //简介
+                    if (!TextUtils.isEmpty(desc)){
+                        user.setDesc(desc);
+                    }else {
+                        user.setDesc("这个人很懒，什么也没有留下。");
+                    }
+                    BmobUser bmobUser = BmobUser.getCurrentUser();
+                    user.update(bmobUser.getObjectId(),new UpdateListener() {
+                        @Override
+                        public void done(BmobException e) {
+                            if(e==null){
+                                setEnabled(false);
+                                btn_update_ok.setVisibility(View.GONE);
+                                Toast.makeText(getActivity(), "修改成功", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(getActivity(), "修改失败", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }else {
+                    Toast.makeText(getActivity(), "输入框不能为空", Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+        }
     }
 }
